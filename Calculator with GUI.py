1d4c8b2fdc8256125bc2baeb6d7bc04e3bef82f4
Vincent Johnson <103472740+Crosstowndonkey@users.noreply.github.com>
2024-06-10 16:08:51 +0800

    Add files via upload
    My first go at a Calculator with GUI using python tkinter.

---
+++ Calculator with GUI.py
@@ -0,0 +1,18 @@
+#Calculator with GUI
+
+import tkinter as tk
+
+calculation = ""
+
+def add_to_calculation(symbol):
+    pass
+
+def evaluate_calculation():
+    pass
+
+def clear_field():
+    pass
+
+root = tk.tk()
+root.geometry("300x275")
+root.mainloop()
